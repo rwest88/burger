c29840f3264f726614d60ef9bcd9d74438028a2c
Ryan Weston <rwest88.va@gmail.com>
2018-07-14 22:00:41 -0400

    more

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('The Chuck (Chicken-Duck)');
+INSERT INTO burgers (name) VALUES ('The Atomic Hotdog Tomahawk Slam Dunk');
+INSERT INTO burgers (name) VALUES ('The Swiss Cheese Philodendron Burger');
+INSERT INTO burgers (name) VALUES ('The Deep-Fried Ball of Breaded Mayonnaise Burger');
+INSERT INTO burgers (name) VALUES ('The Cinnamon-Toast-Crunch Chicken-Nugget Sushi-Burrito Pizza Burger');
+INSERT INTO burgers (name) VALUES ('The McJank');
